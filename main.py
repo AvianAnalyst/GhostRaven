ccfd5ed488a274470d9987bb341d64be09d8662e
Kylan Byrd (First Information Technology) <v-kybyrd@microsoft.com>
2021-05-11 16:29:50 -0700

    Updated AC-07 language for alternative implementation

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ with open('contents/MTOnly.yaml') as environment_contents:
 mt_builder = SSPBuilder('templates/MTFinalSSP_Template.docx', mt_document_contents.contents)
 mt_builder.build_ssp()
 mt_builder.save('output/mt_output.docx')
-#
-# with open('contents/Universal.yaml') as universal_contents, open('templates/gcch_checkbox_templates.yaml') as checkboxes:
-#     gcch_document_contents = ContentBuilder(checkboxes, universal_contents)
-# with open('contents/GCCHOnly.yaml') as environment_contents:
-#     gcch_document_contents.add_contents(environment_contents)
-#
-# gcch_builder = SSPBuilder('templates/GCCHFinalSSP_Template.docx', gcch_document_contents.contents)
-# gcch_builder.build_ssp()
-# gcch_builder.save('output/gcch_output.docx')
+
+with open('contents/Universal.yaml') as universal_contents, open('templates/gcch_checkbox_templates.yaml') as checkboxes:
+    gcch_document_contents = ContentBuilder(checkboxes, universal_contents)
+with open('contents/GCCHOnly.yaml') as environment_contents:
+    gcch_document_contents.add_contents(environment_contents)
+
+gcch_builder = SSPBuilder('templates/GCCHFinalSSP_Template.docx', gcch_document_contents.contents)
+gcch_builder.build_ssp()
+gcch_builder.save('output/gcch_output.docx')
 
 with open('contents/universal.yaml') as universal_contents, open('templates/dod_checkbox_templates.yaml') as checkboxes:
     dod_document_contents = ContentBuilder(checkboxes, universal_contents)
